e5cf5381610c7d48f8064c76505046b1c46b3fa6
Nicholas Raphael <nicholasraphaelvalentiuslaw_2428250016@mhs.mdp.ac.id>
2025-10-27 22:14:34 +0700

    commit

--- routes/api.php
+++ routes/api.php
@@ -5,20 +5,21 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\JadwalController;
 use App\Http\Controllers\API\TransportasiController;
 
+
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+
 Route::get('/transportasi', [TransportasiController::class, 'index']);
-Route::get('/transportasi/{id}', [TransportasiController::class, 'show']);
 Route::post('/transportasi', [TransportasiController::class, 'store']);
+Route::get('/transportasi/{id}', [TransportasiController::class, 'show']);
 Route::patch('/transportasi/{id}', [TransportasiController::class, 'update']);
 Route::delete('/transportasi/{id}', [TransportasiController::class, 'destroy']);
 
-
-
 Route::get('/jadwal', [JadwalController::class, 'index']);
-Route::get('/jadwal/{id}', [JadwalController::class, 'show']);
 Route::post('/jadwal', [JadwalController::class, 'store']);
+Route::get('/jadwal/{id}', [JadwalController::class, 'show']);
 Route::patch('/jadwal/{id}', [JadwalController::class, 'update']);
 Route::delete('/jadwal/{id}', [JadwalController::class, 'destroy']);
